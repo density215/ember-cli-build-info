0f9e54fd0d9b5cd2894654c5943a13aac5a3b665
Jasper den Hertog <jasper@plainspace.com>
2015-02-19 22:41:36 +0100

    svn support

--- index.js
+++ index.js
@@ -1,80 +1,97 @@
 /* jshint node: true */
 'use strict';
 
-var execSync = require('exec-sync');
+var execSync = require('exec-sync'),
+    xml2js = require('xml2js').parseString;
 
 module.exports = {
-  name: 'ember-cli-build-info',
+    name: 'ember-cli-build-info',
 
-  /**
-   * Collect Build Info data
-   */
-  included: function(app, parentAddon) {
-    var target = (parentAddon || app);
-    var info;
-    var commit;
+    /**
+     * Collect Build Info data
+     */
+    included: function (app, parentAddon) {
+        var target = (parentAddon || app);
+        var info;
+        var commit;
 
-    var defaultOptions = {
-      metaTemplate: false, // 'VERSION: {VERSION} SHA: {COMMIT}',
-      injectedKey: 'buildInfo'
-    };
+        var defaultOptions = {
+            metaTemplate: false, // 'VERSION: {VERSION} SHA: {COMMIT}',
+            injectedKey: 'buildInfo'
+        };
 
-    this.options = target.options.buildInfoOptions || {};
+        this.options = target.options.buildInfoOptions || {};
 
-    // merge options
-    for (var option in defaultOptions) {
-      if (!this.options.hasOwnProperty(option)) {
-        this.options[option] = defaultOptions[option];
-      }
+        // merge options
+        for (var option in defaultOptions) {
+            if (!this.options.hasOwnProperty(option)) {
+                this.options[option] = defaultOptions[option];
+            }
+        }
+
+        // build info object
+        info = {
+            version: this.project.pkg.version || ''
+        };
+
+        if (this.options.vcsFlavor === 'git') {
+
+            info.desc = execSync('git describe --tags --long --always') || null;
+            if (typeof info.desc === 'string') {
+                commit = info.desc.split('-').pop();
+
+                // remove the prepended `g`
+                if (commit[0] === 'g') {
+                    commit = commit.substr(1);
+                }
+
+                info.commit = commit;
+            }
+        }
+
+        if (this.options.vcsFlavor === 'svn') {
+            var svn = execSync('svn info ' + this.options.svnURL) || null,
+                descObj = {};
+            svn.split('\n').forEach(function (l) {
+                var kv = l.split(': ');
+                descObj[kv[0]] = kv[1];
+            });
+            info.desc = 'rev-' + descObj.Revision;
+            info.commit = descObj['Repository UUID'];
+        }
+        // store the info
+        this.info = info;
+
+    },
+
+    /**
+     * Expose the data on the APP object.
+     * FIXME: I doubt this is the best way to do this..
+     */
+    config: function (env, config) {
+        config.APP.BUILD_INFO = this.info;
+    },
+
+    /**
+     * Inject a <meta> tag with the build info as the content.
+     */
+    contentFor: function (type) {
+        var info = this.info;
+        var options = this.options;
+        var output;
+
+        if (type === 'head') {
+            //abort meta tag injection if there's no template
+            if (!options.metaTemplate) {
+                return;
+            }
+
+            output = options.metaTemplate
+                .replace(/\{VERSION\}/g, info.version)
+                .replace(/\{DESC\}/g, info.desc)
+                .replace(/\{COMMIT\}/g, info.commit);
+
+            return '<meta name="build-info" content="' + output + '"/>';
+        }
     }
-
-    // build info object
-    info = {
-      version: this.project.pkg.version || '',
-      desc: execSync('git describe --tags --long --always') || null
-    };
-
-    if (typeof info.desc === 'string') {
-      commit = info.desc.split('-').pop();
-
-      // remove the prepended `g`
-      if (commit[0] === 'g') {
-        commit = commit.substr(1);
-      }
-
-      info.commit = commit;
-    }
-
-    // store the info
-    this.info = info;
-  },
-
-  /**
-   * Expose the data on the APP object.
-   * FIXME: I doubt this is the best way to do this..
-   */
-  config: function(env, config) {
-    config.APP.BUILD_INFO = this.info;
-  },
-
-  /**
-   * Inject a <meta> tag with the build info as the content.
-   */
-  contentFor: function(type) {
-    var info    = this.info;
-    var options = this.options;
-    var output;
-
-    if (type === 'head') {
-      //abort meta tag injection if there's no template
-      if (!options.metaTemplate) { return; }
-
-      output = options.metaTemplate
-        .replace(/\{VERSION\}/g, info.version)
-        .replace(/\{DESC\}/g, info.desc)
-        .replace(/\{COMMIT\}/g, info.commit);
-
-      return '<meta name="build-info" content="' + output + '"/>';
-    }
-  }
 };
